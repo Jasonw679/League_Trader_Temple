078d9e13389aa31aa3970025768bccc2799fed1d
Jason Wong <73761709+Jasonw679@users.noreply.github.com>
2026-05-03 18:21:43 -0700

    Optimize Web

---
+++ league_trader_temple.client/src/app/home/home.ts
@@ -0,0 +1,42 @@
+import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RiftboundCard, RiftboundCardPage } from '../app';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  public readonly cards = signal<RiftboundCard[]>([]);
+  public readonly cardLoadError = signal('');
+  public readonly isLoadingCards = signal(false)
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit(): void {
+    this.loadCards();
+  }
+
+  public loadCards(): void {
+    const params: Record<string, string | number> = {
+      size: 10,
+      sort: 'collector_number'
+    };
+
+    this.isLoadingCards.set(true);
+    this.cardLoadError.set('');
+
+    this.http.get<RiftboundCardPage>('/riftboundcards', { params }).subscribe({
+      next: (page) => {
+        this.cards.set(page.items);
+        this.isLoadingCards.set(false);
+      },
+      error: () => {
+        this.cards.set([]);
+        this.cardLoadError.set('Unable to load Riftbound cards right now.');
+        this.isLoadingCards.set(false);
+      }
+    });
+  }
+}
---
+++ league_trader_temple.client/src/app/search/search.ts
@@ -0,0 +1,48 @@
+import { Component, signal, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { RiftboundCardPage, RiftboundCard } from '../app';
+
+@Component({
+  selector: 'app-search',
+  standalone: false,
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+})
+export class Search implements OnInit {
+  public readonly cards = signal<RiftboundCard[]>([]);
+  public readonly isLoadingCards = signal(false);
+  public readonly cardLoadError = signal('');
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  public ngOnInit(): void {
+    const search = this.route.snapshot.queryParamMap.get('search') ?? '';
+    this.loadCards(search);
+  }
+
+  public loadCards(search = ''): void {
+    const params: Record<string, string | number> = {
+      size: 10,
+      sort: 'collector_number'
+    };
+
+    if (search) {
+      params['search'] = search;
+    }
+
+    this.isLoadingCards.set(true);
+    this.cardLoadError.set('');
+
+    this.http.get<RiftboundCardPage>('/riftboundcards', { params }).subscribe({
+      next: (page) => {
+        this.cards.set(page.items);
+        this.isLoadingCards.set(false);
+      },
+      error: () => {
+        this.cards.set([]);
+        this.cardLoadError.set('Unable to load Riftbound cards right now.');
+        this.isLoadingCards.set(false);
+      }
+    });
+  }
+}
